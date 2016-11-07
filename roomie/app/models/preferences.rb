9ce01d635ddbf1dce40f3418e5c788668d109f03
Guy Yardeni <gy2241@columbia.edu>
2016-11-07 10:30:47 -0500

    add validation around postal code for preferences and profile

--- roomie/app/models/preferences.rb
+++ roomie/app/models/preferences.rb
@@ -1,4 +1,4 @@
 class Preferences < ApplicationRecord
-	validates :postal_code,:allow_blank => true, :numericality => {:only_integer => true}
+	validates :postal_code,:allow_blank => true, :numericality => {:only_integer => true, :greater_than => 0}, :length => { is: 5 }
 	belongs_to :user
 end
--- roomie/app/models/profile.rb
+++ roomie/app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
 	validates :user_name, presence: true, uniqueness: true
-	validates :postal_code,:allow_blank => true, :numericality => {:only_integer => true}
+	validates :postal_code,:allow_blank => true, :numericality => {:only_integer => true, :greater_than => 0}, :length => { is: 5 }
 	belongs_to :user
 end
