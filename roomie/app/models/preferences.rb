68207098cd77b39e2c539f3e4f8d15bc31e2e6db
Guy Yardeni <gy2241@columbia.edu>
2016-11-07 14:41:56 -0500

    add validation with allowed regex for street and city to preferences model. All preferences tests are now passing.

--- roomie/app/models/preferences.rb
+++ roomie/app/models/preferences.rb
@@ -1,4 +1,6 @@
 class Preferences < ApplicationRecord
 	validates :postal_code,:allow_blank => true, :numericality => {:only_integer => true, :greater_than => 0}, :length => { is: 5 }
+	validates :street, :format => {:with => /\A[a-zA-Z\d\-\s]+\z/, :message => "only allows letters, spaces, numbers and dashes"}, :if => :looking_for_residence
+	validates :city, :format => {:with => /\A[a-zA-Z\d\-\s]+\z/, :message => "only allows letters, spaces, numbers and dashes"}, :if => :looking_for_residence
 	belongs_to :user
 end
