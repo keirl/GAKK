3328e0af5e81da0c4223fce93780adfa78673a83
akila <you@example.com>
2016-11-15 14:06:39 -0500

    add a seed user with a profile

--- roomie/db/seeds.rb
+++ roomie/db/seeds.rb
@@ -9,4 +9,20 @@
 user = User.create! :email => 'guy@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret',:confirmed_at => Time.now
 user = User.create! :email => 'keirl@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now
 user = User.create! :email => 'kourtney@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now
-user = User.create! :email => 'akila@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now
+user4 = User.create! :email => 'akila@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :confirmed_at => Time.now
+
+Profile.create(user_name: 'akila3',
+               gender: 'Male',
+               is_a_smoker: false,
+               pet_friendly: false,
+               cleanliness_level: 1,
+               outgoingness_level: 1,
+               quietness_level: 1,
+               has_residence_already: true,
+               street: '500 Riverside drive',
+               city: 'New York',
+               state: 'NY',
+               postal_code: '10027',
+               user_id: user4.id,
+               latitude: nil,
+               longitude: nil)
