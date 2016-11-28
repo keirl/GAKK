4bc6bbe8c06e730c2c0e8cad3ec355ddd0357800
Guy Yardeni <gy2241@columbia.edu>
2016-11-28 16:18:13 -0500

    update production mail

--- roomie/config/environments/production.rb
+++ roomie/config/environments/production.rb
@@ -81,6 +81,17 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => "587",
+     :domain => "gmail.com",
+     :authentication => :login,
+     :user_name => "",
+     :password => ""
+  }
+  config.action_mailer.default_url_options = { :host => 'roomiematcher.xyz' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
