004640c2b419bf94c7fec4ec723fffb6717bd7a1
Guy Yardeni <gy2241@columbia.edu>
2016-12-03 18:45:24 -0500

    remove some unused code in matches controller

--- roomie/app/controllers/matches_controller.rb
+++ roomie/app/controllers/matches_controller.rb
@@ -17,7 +17,6 @@ class MatchesController < ApplicationController
 
   # GET /matches/new
   def new
-    @match = Match.new
   end
 
   # GET /matches/1/edit
@@ -27,41 +26,16 @@ class MatchesController < ApplicationController
   # POST /matches
   # POST /matches.json
   def create
-    @match = Match.new(match_params)
-
-    respond_to do |format|
-      if @match.save
-        format.html { redirect_to @match, notice: 'Match was successfully created.' }
-        format.json { render :show, status: :created, location: @match }
-      else
-        format.html { render :new }
-        format.json { render json: @match.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /matches/1
   # PATCH/PUT /matches/1.json
   def update
-    respond_to do |format|
-      if @match.update(match_params)
-        format.html { redirect_to @match, notice: 'Match was successfully updated.' }
-        format.json { render :show, status: :ok, location: @match }
-      else
-        format.html { render :edit }
-        format.json { render json: @match.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /matches/1
   # DELETE /matches/1.json
   def destroy
-    @match.destroy
-    respond_to do |format|
-      format.html { redirect_to matches_url, notice: 'Match was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -72,6 +46,5 @@ class MatchesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def match_params
-      params.require(:match).permit(:user_id_1, :user_id_2, :percent_match)
     end
 end
