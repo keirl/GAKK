cd8f36720307bac061bafca799e393cde96f1307
akila <you@example.com>
2016-12-04 09:34:40 -0500

    some refactoring in the images : still looks ugly

--- roomie/app/controllers/pictures_controller.rb
+++ roomie/app/controllers/pictures_controller.rb
@@ -57,10 +57,11 @@ class PicturesController < ApplicationController
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
+
     @picture = Picture.find(params[:id])
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to :back, notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- roomie/app/views/images/_image.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! image, :id, :caption, :created_at, :updated_at
-json.url image_url(image, format: :json)
